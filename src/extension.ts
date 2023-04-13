f08758521aaa06c8969f0f3bc13ed218dbfa3278
Ethan Strominger <ethanstrominger2@gmail.com>
2023-04-13 15:57:33 -0400

    Hello world
    Co-authored-by: Joel Silberman <42779942+jcs-instructor@users.noreply.github.com>

--- src/extension.ts
+++ src/extension.ts
@@ -1,55 +1,32 @@
-/*---------------------------------------------------------
- * Copyright (C) Microsoft Corporation. All rights reserved.
- *--------------------------------------------------------*/
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from "vscode";
 
-// todo: see tutorial on statusbar control: https://vscode-docs.readthedocs.io/en/stable/extensions/example-word-count/
-/*
-Co-authored-by: Ethan Strominger <ethanstrominger2@gmail.com> 
-Co-authored-by: Joel Silberman <42779942+jcs-instructor@users.noreply.github.com>
-*/
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(
+    'Congratulations, your extension "helloworld-vscode-extension" is now active!'
+  );
 
-import * as vscode from 'vscode';
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    "helloworld-vscode-extension.helloWorld",
+    () => {
+      // The code you place here will be executed every time your command is executed
+      // Display a message box to the user
+      vscode.window.showInformationMessage(
+        "Hello World from helloworld-vscode-extension!"
+      );
+    }
+  );
 
-let myStatusBarItem: vscode.StatusBarItem;
-
-export function activate({ subscriptions }: vscode.ExtensionContext) {
-
-	// register a command that is invoked when the status bar
-	// item is selected
-	const myCommandId = 'sample.showSelectionCount';
-	subscriptions.push(vscode.commands.registerCommand(myCommandId, () => {
-		const n = getNumberOfSelectedLines(vscode.window.activeTextEditor);
-		vscode.window.showInformationMessage(`Yeah, ${n} line(s) selected... Keep going!`);
-	}));
-
-	// create a new status bar item that we can now manage
-	myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
-	myStatusBarItem.command = myCommandId;
-	subscriptions.push(myStatusBarItem);
-
-	// register some listener that make sure the status bar 
-	// item always up-to-date
-	subscriptions.push(vscode.window.onDidChangeActiveTextEditor(updateStatusBarItem));
-	subscriptions.push(vscode.window.onDidChangeTextEditorSelection(updateStatusBarItem));
-
-	// update status bar item once at start
-	updateStatusBarItem();
+  context.subscriptions.push(disposable);
 }
 
-function updateStatusBarItem(): void {
-	const n = getNumberOfSelectedLines(vscode.window.activeTextEditor);
-	if (n > 0) {
-		myStatusBarItem.text = `$(megaphone) ${n} line(s) selected (abc)`;
-		myStatusBarItem.show();
-	} else {
-		myStatusBarItem.hide();
-	}
-}
-
-function getNumberOfSelectedLines(editor: vscode.TextEditor | undefined): number {
-	let lines = 0;
-	if (editor) {
-		lines = editor.selections.reduce((prev, curr) => prev + (curr.end.line - curr.start.line), 0);
-	}
-	return lines;
-}
+// This method is called when your extension is deactivated
+export function deactivate() {}
