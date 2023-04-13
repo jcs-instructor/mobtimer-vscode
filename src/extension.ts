dbdd7bcba7ee5b3a0d4250ef4437c22d50d755ef
Ethan Strominger <ethanstrominger2@gmail.com>
2023-04-13 16:57:12 -0400

    Update status bar - simplify
    Co-authored-by: Joel Silberman <42779942+jcs-instructor@users.noreply.github.com>

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,12 @@
-import {window, commands, Disposable, ExtensionContext, StatusBarAlignment, StatusBarItem, TextDocument} from 'vscode';
+import {
+  window,
+  commands,
+  Disposable,
+  ExtensionContext,
+  StatusBarAlignment,
+  StatusBarItem,
+  TextDocument,
+} from "vscode";
 
 // Your extension is activated the very first time the command is executed
 export function activate(context: ExtensionContext) {
@@ -9,14 +17,11 @@ export function activate(context: ExtensionContext) {
   let wordCounter = new WordCounter();
 
   // The commandId parameter must match the command field in package.json
-  let disposable = commands.registerCommand(
-    "mobtimer.helloWorld",
-    () => {
-		wordCounter.updateWordCount();	
-		console.log("Hello World from mobtimer-vscode!!");	
-		//window.showInformationMessage("Hello World from mobtimer-vscode!!");
-    }
-  );
+  let disposable = commands.registerCommand("mobtimer.helloWorld", () => {
+    wordCounter.updateWordCount();
+    console.log("Hello World from mobtimer-vscode!!");
+    //window.showInformationMessage("Hello World from mobtimer-vscode!!");
+  });
 
   context.subscriptions.push(wordCounter);
   context.subscriptions.push(disposable);
@@ -25,54 +30,18 @@ export function activate(context: ExtensionContext) {
 export function deactivate() {}
 
 class WordCounter {
+  private _statusBarItem: StatusBarItem;
 
-    private _statusBarItem: StatusBarItem;
+  public constructor() {
+    this._statusBarItem = window.createStatusBarItem(StatusBarAlignment.Left);
+  }
 
-	public constructor() {
-		this._statusBarItem = window.createStatusBarItem(StatusBarAlignment.Left); 
-	}
+  public updateWordCount() {
+    this._statusBarItem.text = "(not a markdown file)";
+    this._statusBarItem.show();
+  }
 
-    public updateWordCount() {
-
-        // Get the current text editor 
-        let editor = window.activeTextEditor; 
-        if (!editor) { 
-            this._statusBarItem.hide(); 
-            return; 
-        } 
-
-         let doc = editor.document; 
-
-        // Only update status if an MarkDown file 
-        if (doc.languageId === "markdown") { 
-            let wordCount = this._getWordCount(doc); 
-
-            // Update the status bar 
-            this._statusBarItem.text = "yadadadadyayaya"; //wordCount !== 1 ? `${wordCount} Words` : '1 Word'; 
-            this._statusBarItem.show(); 
-        } else { 
-            this._statusBarItem.text = "(not a markdown file)"; 
-            this._statusBarItem.show(); 
-            // this._statusBarItem.hide(); 
-        } 
-    } 
-
-    public _getWordCount(doc: TextDocument): number { 
-
-        let docContent = doc.getText(); 
-
-        // Parse out unwanted whitespace so the split is accurate 
-        docContent = docContent.replace(/(< ([^>]+)<)/g, '').replace(/\s+/g, ' '); 
-        docContent = docContent.replace(/^\s\s*/, '').replace(/\s\s*$/, ''); 
-        let wordCount = 0; 
-        if (docContent !== "") { 
-            wordCount = docContent.split(" ").length; 
-        } 
-
-        return wordCount; 
-    } 
-
-    dispose() {
-        this._statusBarItem.dispose();
-    }
+  dispose() {
+    this._statusBarItem.dispose();
+  }
 }
