de100ddfbc5f2b575d543d29a9ad9581af973636
jcs-instructor <jcsilberman@gmail.com>
2023-04-15 11:28:47 -0400

    Pad time with zeroes, & increase visibility in statusbar

--- src/extension.ts
+++ src/extension.ts
@@ -1,3 +1,4 @@
+import { SERVFAIL } from "dns";
 import {
   window,
   commands,
@@ -27,7 +28,7 @@ export function activate(context: ExtensionContext) {
   context.subscriptions.push(disposable);
 }
 
-export function deactivate() {}
+export function deactivate() { }
 
 class Timer {
   private _statusBarItem: StatusBarItem;
@@ -36,15 +37,24 @@ class Timer {
     this._statusBarItem = window.createStatusBarItem(StatusBarAlignment.Left);
   }
 
-  public update() {	
-	// Every second, update the status bar with the current time with seconds
-	setInterval(() => {
-		const date = new Date();
-		const time = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
-		this._statusBarItem.text = `$(clock) ${time}`;
-	}, 1000);
-    //this._statusBarItem.text = "Timer: $(clock) 00:00";
+  public update() {
+    // Every second, update the status bar with the current time with seconds
+    setInterval(
+      () => {
+        const text = `[ $(clock) CURRENT TIME: ${this.getCurrentTime()} ]`;
+        this._statusBarItem.text = text;
+      },
+      1000); // 1000 ms = 1 second 
     this._statusBarItem.show();
+  }
+
+  private getCurrentTime() {
+    let date = new Date();
+    let hours = date.getHours().toString().padStart(2, "0");
+    let minutes = date.getMinutes().toString().padStart(2, "0");
+    let seconds = date.getSeconds().toString().padStart(2, "0");
+    let currentTime = `${hours}:${minutes}:${seconds}`;
+    return currentTime;
   }
 
   dispose() {
