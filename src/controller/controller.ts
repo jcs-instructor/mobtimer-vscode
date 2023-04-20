0ba8270b043003d7680b1c10d12d5e5822a6024a
jcs-instructor <jcsilberman@gmail.com>
2023-04-20 16:52:44 -0400

    Commented out client in controller
    Co-authored-by: Ethan Strominger <ethanstrominger2@gmail.com>

--- src/controller/controller.ts
+++ src/controller/controller.ts
@@ -1,10 +1,10 @@
+import { Status } from "../mobtimer-api-copy/status";
 import { MobTimer } from "../mobtimer-api-copy/mobTimer";
-// import { Status } from "../mobtimer-api-copy/status";
 // import * as MobTimerRequests from "../mobtimer-api-copy/mobTimerRequests";
-// import * as MobTimerResponses from "../mobtimer-api-copy/mobTimerResponse";
+import * as MobTimerResponses from "../mobtimer-api-copy/mobTimerResponse";
 // import { MobSocketClient } from "../mobtimer-api-copy";
 
-// // todo: unhardcode port
+// todo: unhardcode port
 // const url =
 //   process.env.REACT_APP_WEBSOCKET_URL ||
 //   `ws://localhost:${process.env.REACT_APP_WEBSOCKET_PORT || "4000"}`;
@@ -23,63 +23,63 @@ export class Controller {
     };
   }
 
-  // static getAppTitle() {
-  //   return "Mob Timer";
-  // }
+  static getAppTitle() {
+    return "Mob Timer";
+  }
 
-  // // injections -----------------------
+  // injections -----------------------
 
-  // // inject duration minutes
-  // static setDurationMinutes = (_durationMinutes: number) => {}; // todo: consider alternatives to putting an underscore in the name; e.g., try abstract method/class, or interface
-  // static injectSetDurationMinutes(
-  //   setDurationMinutesFunction: (durationMinutes: number) => void
-  // ) {
-  //   this.setDurationMinutes = setDurationMinutesFunction;
-  // }
+  // inject duration minutes
+  static setDurationMinutes = (_durationMinutes: number) => {}; // todo: consider alternatives to putting an underscore in the name; e.g., try abstract method/class, or interface
+  static injectSetDurationMinutes(
+    setDurationMinutesFunction: (durationMinutes: number) => void
+  ) {
+    this.setDurationMinutes = setDurationMinutesFunction;
+  }
 
-  // // inject time string
-  // static setSecondsRemainingString = (_timeString: string) => {}; // todo: consider alternatives to putting an underscore in the name; e.g., try abstract method/class, or interface
-  // static injectSetSecondsRemainingString(
-  //   setSecondsRemainingStringFunction: (timeString: string) => void
-  // ) {
-  //   this.setSecondsRemainingString = setSecondsRemainingStringFunction;
-  // }
+  // inject time string
+  static setSecondsRemainingString = (_timeString: string) => {}; // todo: consider alternatives to putting an underscore in the name; e.g., try abstract method/class, or interface
+  static injectSetSecondsRemainingString(
+    setSecondsRemainingStringFunction: (timeString: string) => void
+  ) {
+    this.setSecondsRemainingString = setSecondsRemainingStringFunction;
+  }
 
-  // // inject participants
-  // static setParticipants = (_participants: string[]) => {}; // todo: consider alternatives to putting an underscore in the name; e.g., try abstract method/class, or interface
-  // static injectSetParticipants(
-  //   setParticipantsFunction: (participants: string[]) => void
-  // ) {
-  //   this.setParticipants = setParticipantsFunction;
-  // }
+  // inject participants
+  static setParticipants = (_participants: string[]) => {}; // todo: consider alternatives to putting an underscore in the name; e.g., try abstract method/class, or interface
+  static injectSetParticipants(
+    setParticipantsFunction: (participants: string[]) => void
+  ) {
+    this.setParticipants = setParticipantsFunction;
+  }
 
-  // // other functions -----------------------
+  // other functions -----------------------
 
-  // static translateResponseData(response: MobTimerResponses.SuccessfulResponse) {
-  //   const mobState = response.mobState;
-  //   const mobStatus = mobState.status;
-  //   const durationMinutes = mobState.durationMinutes;
-  //   const participants = mobState.participants;
-  //   const secondsRemaining = mobState.secondsRemaining;
-  //   return { mobStatus, durationMinutes, participants, secondsRemaining };
-  // }
+  static translateResponseData(response: MobTimerResponses.SuccessfulResponse) {
+    const mobState = response.mobState;
+    const mobStatus = mobState.status;
+    const durationMinutes = mobState.durationMinutes;
+    const participants = mobState.participants;
+    const secondsRemaining = mobState.secondsRemaining;
+    return { mobStatus, durationMinutes, participants, secondsRemaining };
+  }
 
-  // static getActionButtonLabel(backendStatus: Status) {
-  //   switch (backendStatus) {
-  //     case Status.Running: {
-  //       return "⏸️ Pause";
-  //     }
-  //     case Status.Paused: {
-  //       return "▶️ Resume";
-  //     }
-  //     case Status.Ready: {
-  //       return "▶️ Start";
-  //     }
-  //     default: {
-  //       return "";
-  //     } // todo: maybe handle invalid status differently
-  //   }
-  // }
+  static getActionButtonLabel(backendStatus: Status) {
+    switch (backendStatus) {
+      case Status.Running: {
+        return "⏸️ Pause";
+      }
+      case Status.Paused: {
+        return "▶️ Resume";
+      }
+      case Status.Ready: {
+        return "▶️ Start";
+      }
+      default: {
+        return "";
+      } // todo: maybe handle invalid status differently
+    }
+  }
 
   // static toggle(client: MobSocketClient, frontendMobtimer: MobTimer) {
   //   switch (frontendMobtimer.status) {
@@ -101,22 +101,22 @@ export class Controller {
   //   }
   // }
 
-  // static changeStatus(frontendMobtimer: MobTimer, backendStatus: Status) {
-  //   if (frontendMobtimer.status !== backendStatus) {
-  //     switch (backendStatus) {
-  //       case Status.Running: {
-  //         frontendMobtimer.start();
-  //         break;
-  //       }
-  //       case Status.Paused: {
-  //         frontendMobtimer.pause();
-  //         break;
-  //       }
-  //       case Status.Ready: {
-  //         frontendMobtimer.reset();
-  //         break;
-  //       }
-  //     }
-  //   }
-  // }
+  static changeStatus(frontendMobtimer: MobTimer, backendStatus: Status) {
+    if (frontendMobtimer.status !== backendStatus) {
+      switch (backendStatus) {
+        case Status.Running: {
+          frontendMobtimer.start();
+          break;
+        }
+        case Status.Paused: {
+          frontendMobtimer.pause();
+          break;
+        }
+        case Status.Ready: {
+          frontendMobtimer.reset();
+          break;
+        }
+      }
+    }
+  }
 }
