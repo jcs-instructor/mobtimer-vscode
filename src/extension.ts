3fa5a4e8b3ef882e71617d488fb6cc7d13ca6835
jcs-instructor <jcsilberman@gmail.com>
2023-04-20 16:07:04 -0400

    Refactor
    Co-authored-by: Ethan Strominger <ethanstrominger2@gmail.com>

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,11 @@
 import { SERVFAIL } from "dns";
 import {
-  window,
   commands,
   Disposable,
   ExtensionContext,
-  StatusBarAlignment,
-  StatusBarItem,
   TextDocument,
 } from "vscode";
+import { Timer } from "./timer";
 
 // todo: in progress
 //import { Controller } from './controller/controller';
@@ -81,34 +79,4 @@ export async function activate(context: ExtensionContext) {
 
 export function deactivate() { }
 
-class Timer {
-  private _statusBarItem: StatusBarItem;
 
-  public constructor() {
-    this._statusBarItem = window.createStatusBarItem(StatusBarAlignment.Left);
-  }
-
-  public update() {
-    // Every second, update the status bar with the current time with seconds
-    setInterval(
-      () => {
-        const text = `[ $(clock) CURRENT TIME: ${this.getCurrentTime()} ]`;
-        this._statusBarItem.text = text;
-      },
-      1000); // 1000 ms = 1 second 
-    this._statusBarItem.show();
-  }
-
-  private getCurrentTime() {
-    let date = new Date();
-    let hours = date.getHours().toString().padStart(2, "0");
-    let minutes = date.getMinutes().toString().padStart(2, "0");
-    let seconds = date.getSeconds().toString().padStart(2, "0");
-    let currentTime = `${hours}:${minutes}:${seconds}`;
-    return currentTime;
-  }
-
-  dispose() {
-    this._statusBarItem.dispose();
-  }
-}
